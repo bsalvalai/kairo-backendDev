5c0ab8c55f5395928b0139e390280bc8d1f5caa4
bsalvalai <165174932+bsalvalai@users.noreply.github.com>
2025-10-16 17:02:42 -0300

    Comment out dotenv and path imports
    Comment out dotenv and path imports along with their usage.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-const path = require('path');
+//const dotenv = require('dotenv');
+//const path = require('path');
 
 //dotenv.config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
