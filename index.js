689e07aaa8209182bcae587d755270f4163def39
bsalvalai <165174932+bsalvalai@users.noreply.github.com>
2025-10-16 08:13:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+//dotenv.config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
